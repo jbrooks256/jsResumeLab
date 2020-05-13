88ec1c7cc12f6740620ddbf394b08f80e2083fb5
Justin Brooks <justinbrooks@Justins-MacBook-Pro.local>
2020-05-13 15:12:43 -0500

    finished js code

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-let resume = {
-    Name: "Justin Brooks",
-    Career: "Professional Professional",
-    Description: "Yes",
-    Interests: "Reading, Video Games, Music",
-    Experience: [
-        "Math, Chemistry, Biology",
-        "Undergraduate Teaching Assistant at University of Georgia - Helped grade assignments, answer questions, held lab hours, and held office hours for Java and C++ Computer Science classes.",
-        "Lead Programmer for iBeacon Experiment at Georgia Museum of Art - Created native iOS virtual tour guide that used iBeacon technology to triangulate position within the museum and give information about a piece."
-    ],
-    Skills: ["Walking", "Eating", "Talking", "Breathing"]
+console.log("Name: " + "Justin Brooks".toUpperCase());
+console.log("Career: Professional Professional");
+console.log("Description: Yes");
+console.log("\n");
+console.log("My Interests:");
+console.log("* Reading");
+console.log("* Video Games");
+console.log("* Music");
+console.log("\n");
+displayPosition("Math, Chemistry, Physics, Biology, and English tutor", "Some School", "Tutored students in those areas.");
+displayPosition("Undergraduate Teaching Assistant", "University of Georgia", "Helped grade assignments, answer questions, held lab hours, and held office hours for Java and C++ Computer Science classes.");
+displayPosition("Lead Programmer for iBeacon Experiment", "Georgia Museum of Art", "Created native iOS virtual tour guide that used iBeacon technology to triangulate position within the museum and give information about a piece.");
+console.log("\n");
+console.log("My Skills:");
+displaySkill("Walking", true);
+displaySkill("Eating", false);
+displaySkill("Talking", false);
+displaySkill("Breathing", true);
+
+function displayPosition(jobTitle, companyName, description) {
+    console.log(jobTitle + " at " + companyName + " - " + description);
 };
 
-let allCaps = resume.Name.toUpperCase();
-
-function displayPosition() {
-    
-};
-
-function displaySkill() {
-    for (let i = 0; i < resume.Skills.length; i++) {
-        if (i>1) {
-            console.log("BAM: " + resume.Skills[i]);
-        }   else {
-            console.log(resume.Skills[i]);
-        }
+function displaySkill(mySkill, isCool) {
+    if (isCool == true) {
+        console.log("* BAM: " + mySkill)
+    } else {
+        console.log("* " + mySkill)
     }
-}
-
-console.log((resume),(displaySkill()));
+};   
